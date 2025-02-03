65952f045e4889ec725742640034a8dadc9f70d1
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2025-02-03 17:14:26 +0400

    feat: add test data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import VirtualGrid from '@/components/VirtualGrid/VirtualGrid.vue'
 
+const items = Array.from({ length: 100 }, (_, idx) => `Item ${idx + 1}`)
 </script>
 
 <template>
-  <div class="text-amber-500">hello from home</div>
+  <VirtualGrid :items="items" />
 </template>
