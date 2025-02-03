fd11eaf4f3f6b867bb2a3fcec90916cdd92d82f0
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2025-02-03 17:10:42 +0400

    feat: add base component template and scss

---
+++ src/components/VirtualGrid/VirtualGrid.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts" generic="T">
+import { useTemplateRef } from 'vue'
+
+const {
+  viewportHeight = 500,
+  itemHeight = 250,
+  items,
+} = defineProps<{
+  items: T[]
+  viewportHeight?: number
+  itemHeight?: number
+}>()
+
+const container = useTemplateRef('container')
+</script>
+
+<template>
+  <div
+    class="virtual-grid"
+    ref="container"
+  >
+    <div class="virtual-grid__spacer">
+      <div class="virtual-grid__item"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
